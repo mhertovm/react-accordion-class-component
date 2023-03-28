2c83af9d8500e031228f6513a5445fb79fa06008
mhertovm <mhertovmasyan904@gmail.com>
2023-03-28 12:19:19 +0400

    first commit

---
+++ src/Accordion.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Accordion extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            isActive: false,
+        };
+      }
+      
+    change = () => {this.state.isActive ? this.setState({isActive : false}) : this.setState({isActive : true})}
+
+    render(){
+        return(
+            <div>
+                <div onClick={this.change} className="accordion">
+                    <div>{this.props.name}</div>
+                    <div>{this.state.isActive ? '-' : '+'}</div>
+                </div>
+                {this.state.isActive && <div style={{color:"red"}}>{this.props.capital}</div>}
+            </div>
+        )
+    }
+}
+
+export default Accordion;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./Accordion"
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      countrys : []
+    };
+  }
+  componentDidMount() {
+    fetch('https://countriesnow.space/api/v0.1/countries/capital')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        countrys: res.data
+      })
+    })
+  }
+  render(){
+    return (
+      this.state.countrys.map((value, index)=>{
+        return(
+          <div className="App">
+          <Accordion name={value.name} capital={value.capital}/>
+          </div>
+        )})
+    )
+  }
 }
 
 export default App;
